e7eb29be987f93c41c335aca2a6f88192e85074d
Miles Gray <mgray16@gmail.com>
2020-09-09 17:01:32 -0500

    Move additional activation modules in

--- layers/activations.py
+++ layers/activations.py
@@ -39,3 +39,41 @@ class Mish(nn.Module):
         Forward pass of the function.
         '''
         return mish(input)
+
+def logcosh(x, y):
+    diff = x - y
+    loss = (diff + 1e-12).cosh().log()
+    return loss.mean()
+
+def xtanh(x, y):
+    diff = x - y
+    loss = diff.tanh() * diff
+    return loss.mean()
+
+def xsigmoid(x, y):
+    diff = x - y
+    loss = 1 + (-diff).exp()
+    loss = loss - diff
+    loss = 2 * diff / loss
+    return loss.mean()
+
+class LogCoshLoss(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, y_t, y_prime_t):
+        return logcosh(y_t, y_prime_t)
+
+class XTanhLoss(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, y_t, y_prime_t):        
+        return xtanh(y_t, y_prime_t)
+
+class XSigmoidLoss(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, y_t, y_prime_t):
+        return xsigmoid(y_t, y_prime_t)
