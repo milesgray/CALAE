5817c9a9c087b9f45ce1828a19602284afa77e67
Miles Gray <mgray16@gmail.com>
2020-10-24 02:12:04 -0500

    Add spectral norm layers from DE GAN and an attention module used in SA GAN

--- layers/attention.py
+++ layers/attention.py
@@ -1,6 +1,46 @@
 import torch
 import torch.nn as nn
 
+from .spectralnorm import SNConv2d
+
+class SAGAN_Attention(nn.Module):    
+    def __init__(self, ch, which_conv=SNConv2d):
+        """A non-local block as used in SA-GAN.
+
+        Args:
+            ch (int): Number of channels/filters
+            which_conv (nn.Module, optional): A 2D Convolutional module to use. Defaults to SNConv2d.
+        """
+        super().__init__()
+        # Channel multiplier
+        self.ch = ch
+        self.which_conv = which_conv
+        self.theta = self.which_conv(
+            self.ch, self.ch // 8, kernel_size=1, padding=0, bias=False)
+        self.phi = self.which_conv(
+            self.ch, self.ch // 8, kernel_size=1, padding=0, bias=False)
+        self.g = self.which_conv(
+            self.ch, self.ch // 2, kernel_size=1, padding=0, bias=False)
+        self.o = self.which_conv(
+            self.ch // 2, self.ch, kernel_size=1, padding=0, bias=False)
+        # Learnable gain parameter
+        self.gamma = P(torch.tensor(0.), requires_grad=True)
+
+    def forward(self, x, y=None):
+        # Apply convs
+        theta = self.theta(x)
+        phi = F.max_pool2d(self.phi(x), [2, 2])
+        g = F.max_pool2d(self.g(x), [2, 2])
+        # Perform reshapes
+        theta = theta.view(-1, self. ch // 8, x.shape[2] * x.shape[3])
+        phi = phi.view(-1, self. ch // 8, x.shape[2] * x.shape[3] // 4)
+        g = g.view(-1, self. ch // 2, x.shape[2] * x.shape[3] // 4)
+        # Matmul and softmax to get attention maps
+        beta = F.softmax(torch.bmm(theta.transpose(1, 2), phi), -1)
+        # Attention map times g path
+        o = self.o(torch.bmm(g, beta.transpose(1, 2)).view(-1,
+                                                           self.ch // 2, x.shape[2], x.shape[3]))
+        return self.gamma * o + x
 
 # ATT_U-Net's Attention module
 class Attention(nn.Module):
