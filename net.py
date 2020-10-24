bc08ac550e9604e40a69e0f9fa1d57c84786ed47
Miles Gray <mgray16@gmail.com>
2020-10-24 12:15:17 -0500

    Fix bugs

--- net.py
+++ net.py
@@ -420,7 +420,28 @@ class LayerNorm(nn.Module):
         x = (x - mean) / (std + self.eps)
 
         if self.affine:
-            shape = [1, -1] + [1] * (x.dim() - 2)
+            shape = [1, -1] + [1] * max((x.dim() - 2), 1)
+            x = x * self.gamma.view(*shape) + self.beta.view(*shape)
+        return x
+class LayerNorm1D(nn.Module):
+    def __init__(self, num_features, eps=1e-5, affine=True):
+        super(LayerNorm1D, self).__init__()
+        self.num_features = num_features
+        self.affine = affine
+        self.eps = eps
+
+        if self.affine:
+            self.gamma = nn.Parameter(torch.Tensor(num_features).uniform_())
+            self.beta = nn.Parameter(torch.zeros(num_features))
+
+    def forward(self, x):
+        shape = [-1] + [1] * (x.dim() - 1)
+        mean = x.view(x.size(0), -1).mean(1).view(*shape)
+        std = x.view(x.size(0), -1).std(1).view(*shape)
+        x = (x - mean) / (std + self.eps)
+
+        if self.affine:
+            shape = [1, -1] + [1]
             x = x * self.gamma.view(*shape) + self.beta.view(*shape)
         return x
 # ------------------------------------------------------------------------------------------------------------------
