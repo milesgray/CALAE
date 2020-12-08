2f0241bbc04aa3fd2d24b4d5ad8da47bbbc5017a
Miles Gray <mgray16@gmail.com>
2020-12-08 05:52:38 -0600

    Fix imports in utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,7 +3,7 @@
 from .alae import find_alpha, allow_gradient
 from .alae import adjust_lr, linear_scale_lr
 from .alae import save_batch, save_reconstructions
-from .noise import sample_noise, set_noise
+from .noise import sample_noise, FAMOS_Noise
 from .density import matrix_log_density_gaussian, log_density_gaussian
 from .importance import log_importance_weight_matrix
 from .files import ensure_dir, ensure_parent_dir
