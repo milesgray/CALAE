4963fa26892cf5136f2d8565275c28ff81fe47fd
Miles Gray <mgray16@gmail.com>
2020-12-10 05:11:13 -0600

    Add TUNIT models and metrics

---
+++ models/tunit.py
@@ -0,0 +1,517 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+from torch import nn
+import numpy as np
+
+
+import math
+
+class ResBlocks(nn.Module):
+    def __init__(self, num_blocks, dim, norm, act, pad_type, use_sn=False):
+        super(ResBlocks, self).__init__()
+        self.model = nn.ModuleList()
+        for i in range(num_blocks):
+            self.model.append(ResBlock(dim, norm=norm, act=act, pad_type=pad_type, use_sn=use_sn))
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class ResBlock(nn.Module):
+    def __init__(self, dim, norm='in', act='relu', pad_type='zero', use_sn=False):
+        super(ResBlock, self).__init__()
+        self.model = nn.Sequential(Conv2dBlock(dim, dim, 3, 1, 1,
+                                               norm=norm,
+                                               act=act,
+                                               pad_type=pad_type, use_sn=use_sn),
+                                   Conv2dBlock(dim, dim, 3, 1, 1,
+                                               norm=norm,
+                                               act='none',
+                                               pad_type=pad_type, use_sn=use_sn))
+
+    def forward(self, x):
+        x_org = x
+        residual = self.model(x)
+        out = x_org + 0.1 * residual
+        return out
+
+
+class ActFirstResBlk(nn.Module):
+    def __init__(self, dim_in, dim_out, downsample=True):
+        super(ActFirstResBlk, self).__init__()
+        self.norm1 = FRN(dim_in)
+        self.norm2 = FRN(dim_in)
+        self.conv1 = nn.Conv2d(dim_in, dim_in, 3, 1, 1)
+        self.conv2 = nn.Conv2d(dim_in, dim_out, 3, 1, 1)
+        self.downsample = downsample
+        self.learned_sc = (dim_in != dim_out)
+        if self.learned_sc:
+            self.conv1x1 = nn.Conv2d(dim_in, dim_out, 1, 1, 0, bias=False)
+
+    def _shortcut(self, x):
+        if self.learned_sc:
+            x = self.conv1x1(x)
+        if self.downsample:
+            x = F.avg_pool2d(x, 2)
+        return x
+
+    def _residual(self, x):
+        x = self.norm1(x)
+        x = self.conv1(x)
+        if self.downsample:
+            x = F.avg_pool2d(x, 2)
+        x = self.norm2(x)
+        x = self.conv2(x)
+        return x
+
+    def forward(self, x):
+        return torch.rsqrt(torch.tensor(2.0)) * self._shortcut(x) + torch.rsqrt(torch.tensor(2.0)) * self._residual(x)
+
+
+class LinearBlock(nn.Module):
+    def __init__(self, in_dim, out_dim, norm='none', act='relu', use_sn=False):
+        super(LinearBlock, self).__init__()
+        use_bias = True
+        self.fc = nn.Linear(in_dim, out_dim, bias=use_bias)
+        if use_sn:
+            self.fc = nn.utils.spectral_norm(self.fc)
+
+        # initialize normalization
+        norm_dim = out_dim
+        if norm == 'bn':
+            self.norm = nn.BatchNorm1d(norm_dim)
+        elif norm == 'in':
+            self.norm = nn.InstanceNorm1d(norm_dim)
+        elif norm == 'none':
+            self.norm = None
+        else:
+            assert 0, "Unsupported normalization: {}".format(norm)
+
+        # initialize activation
+        if act == 'relu':
+            self.activation = nn.ReLU(inplace=True)
+        elif act == 'lrelu':
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        elif act == 'tanh':
+            self.activation = nn.Tanh()
+        elif act == 'none':
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(act)
+
+    def forward(self, x):
+        out = self.fc(x)
+        if self.norm:
+            out = self.norm(out)
+        if self.activation:
+            out = self.activation(out)
+        return out
+
+
+class Conv2dBlock(nn.Module):
+    def __init__(self, in_dim, out_dim, ks, st, padding=0,
+                 norm='none', act='relu', pad_type='zero',
+                 use_bias=True, use_sn=False):
+        super(Conv2dBlock, self).__init__()
+        self.use_bias = use_bias
+
+        # initialize padding
+        if pad_type == 'reflect':
+            self.pad = nn.ReflectionPad2d(padding)
+        elif pad_type == 'replicate':
+            self.pad = nn.ReplicationPad2d(padding)
+        elif pad_type == 'zero':
+            self.pad = nn.ZeroPad2d(padding)
+        else:
+            assert 0, "Unsupported padding type: {}".format(pad_type)
+
+        # initialize normalization
+        norm_dim = out_dim
+        if norm == 'bn':
+            self.norm = nn.BatchNorm2d(norm_dim)
+        elif norm == 'in':
+            self.norm = nn.InstanceNorm2d(norm_dim)
+        elif norm == 'adain':
+            self.norm = AdaIN2d(norm_dim)
+        elif norm == 'none':
+            self.norm = None
+        else:
+            assert 0, "Unsupported normalization: {}".format(norm)
+
+        # initialize activation
+        if act == 'relu':
+            self.activation = nn.ReLU(inplace=True)
+        elif act == 'lrelu':
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        elif act == 'tanh':
+            self.activation = nn.Tanh()
+        elif act == 'none':
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(act)
+
+        self.conv = nn.Conv2d(in_dim, out_dim, ks, st, bias=self.use_bias)
+        if use_sn:
+            self.conv = nn.utils.spectral_norm(self.conv)
+
+    def forward(self, x):
+        x = self.conv(self.pad(x))
+        if self.norm:
+            x = self.norm(x)
+        if self.activation:
+            x = self.activation(x)
+        return x
+
+
+class FRN(nn.Module):
+    def __init__(self, num_features, eps=1e-6):
+        super(FRN, self).__init__()
+        self.tau = nn.Parameter(torch.zeros(1, num_features, 1, 1))
+        self.gamma = nn.Parameter(torch.ones(1, num_features, 1, 1))
+        self.beta = nn.Parameter(torch.zeros(1, num_features, 1, 1))
+        self.eps = eps
+
+    def forward(self, x):
+        x = x * torch.rsqrt(torch.mean(x**2, dim=[2, 3], keepdim=True) + self.eps)
+        return torch.max(self.gamma * x + self.beta, self.tau)
+
+
+class AdaIN2d(nn.Module):
+    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=False, track_running_stats=True):
+        super(AdaIN2d, self).__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+        self.affine = affine
+        self.track_running_stats = track_running_stats
+
+        if self.affine:
+            self.weight = nn.Parameter(torch.Tensor(num_features))
+            self.bias = nn.Parameter(torch.Tensor(num_features))
+        else:
+            self.weight = None
+            self.bias = None
+
+        if self.track_running_stats:
+            self.register_buffer('running_mean', torch.zeros(num_features))
+            self.register_buffer('running_var', torch.ones(num_features))
+        else:
+            self.register_buffer('running_mean', None)
+            self.register_buffer('running_var', None)
+
+    def forward(self, x):
+        assert self.weight is not None and self.bias is not None, "AdaIN params are None"
+        N, C, H, W = x.size()
+        running_mean = self.running_mean.repeat(N)
+        running_var = self.running_var.repeat(N)
+        x_ = x.contiguous().view(1, N * C, H * W)
+        normed = F.batch_norm(x_, running_mean, running_var,
+                              self.weight, self.bias,
+                              True, self.momentum, self.eps)
+        return normed.view(N, C, H, W)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(num_features=' + str(self.num_features) + ')'
+
+# DISCRIMINATOR
+# 
+class Discriminator(nn.Module):
+    """Discriminator: (image x, domain y) -> (logit out)."""
+    def __init__(self, image_size=256, num_domains=2, max_conv_dim=1024):
+        super(Discriminator, self).__init__()
+        dim_in = 64 if image_size < 256 else 32
+        blocks = []
+        blocks += [nn.Conv2d(3, dim_in, 3, 1, 1)]
+
+        repeat_num = int(np.log2(image_size)) - 2
+        for _ in range(repeat_num):
+            dim_out = min(dim_in*2, max_conv_dim)
+            blocks += [ActFirstResBlk(dim_in, dim_in, downsample=False)]
+            blocks += [ActFirstResBlk(dim_in, dim_out, downsample=True)]
+            dim_in = dim_out
+
+        blocks += [nn.LeakyReLU(0.2)]
+        blocks += [nn.Conv2d(dim_out, dim_out, 4, 1, 0)]
+        blocks += [nn.LeakyReLU(0.2)]
+        blocks += [nn.Conv2d(dim_out, num_domains, 1, 1, 0)]
+        self.main = nn.Sequential(*blocks)
+
+        self.apply(weights_init('kaiming'))
+
+    def forward(self, x, y):
+        """
+        Inputs:
+            - x: images of shape (batch, 3, image_size, image_size).
+            - y: domain indices of shape (batch).
+        Output:
+            - out: logits of shape (batch).
+        """
+        out = self.main(x)
+        feat = out
+        out = out.view(out.size(0), -1)                          # (batch, num_domains)
+        idx = torch.LongTensor(range(y.size(0))).to(y.device)
+        out = out[idx, y]                                         # (batch)
+        return out, feat
+
+    def _initialize_weights(self, mode='fan_in'):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode=mode, nonlinearity='relu')
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+
+def weights_init(init_type='gaussian'):
+    def init_fun(m):
+        classname = m.__class__.__name__
+        if (classname.find('Conv') == 0 or classname.find(
+                'Linear') == 0) and hasattr(m, 'weight'):
+            if init_type == 'gaussian':
+                init.normal_(m.weight.data, 0.0, 0.02)
+            elif init_type == 'xavier':
+                init.xavier_normal_(m.weight.data, gain=math.sqrt(2))
+            elif init_type == 'kaiming':
+                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                init.orthogonal_(m.weight.data, gain=math.sqrt(2))
+            elif init_type == 'default':
+                pass
+            else:
+                assert 0, "Unsupported initialization: {}".format(init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant_(m.bias.data, 0.0)
+    return init_fun
+
+
+# GENERATOR
+
+
+
+class Generator(nn.Module):   
+    def __init__(self, img_size=128, sty_dim=64, n_res=2, use_sn=False):
+        super(Generator, self).__init__()
+        print("Init Generator")
+
+        self.nf = 64 if img_size < 256 else 32
+        self.nf_mlp = 256
+
+        self.decoder_norm = 'adain'
+
+        self.adaptive_param_getter = get_num_adain_params
+        self.adaptive_param_assign = assign_adain_params
+
+        print("GENERATOR NF : ", self.nf)
+
+        s0 = 16
+
+        n_downs = int(np.log2(img_size//s0))
+
+        nf_dec = self.nf * 2**n_downs
+
+        self.cnt_encoder = ContentEncoder(self.nf, n_downs, n_res, 'in', 'relu', 'reflect')
+        self.decoder = Decoder(nf_dec, sty_dim, n_downs, n_res, self.decoder_norm, self.decoder_norm, 'relu', 'reflect', use_sn=use_sn)
+        self.mlp = MLP(sty_dim, self.adaptive_param_getter(self.decoder), self.nf_mlp, 3, 'none', 'relu')
+
+        self.apply(weights_init('kaiming'))
+
+    def forward(self, x_src, s_ref):
+        c_src = self.cnt_encoder(x_src)
+        x_out = self.decode(c_src, s_ref)
+        return x_out
+
+    def decode(self, cnt, sty):
+        adapt_params = self.mlp(sty)
+        self.adaptive_param_assign(adapt_params, self.decoder)
+        out = self.decoder(cnt)
+        return out
+
+    def _initialize_weights(self, mode='fan_in'):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode=mode, nonlinearity='relu')
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+
+class Decoder(nn.Module):
+    def __init__(self, nf_dec, sty_dim, n_downs, n_res, res_norm, dec_norm, act, pad, use_sn=False):
+        super(Decoder, self).__init__()
+        print("Init Decoder")
+
+        nf = nf_dec
+        self.model = nn.ModuleList()
+        self.model.append(ResBlocks(n_res, nf, res_norm, act, pad, use_sn=use_sn))
+
+        for _ in range(n_downs):
+            self.model.append(nn.Upsample(scale_factor=2))
+            self.model.append(Conv2dBlock(nf, nf//2, 5, 1, 2, norm=dec_norm, act=act, pad_type=pad, use_sn=use_sn))
+            nf //= 2
+
+        self.model.append(Conv2dBlock(nf, 3, 7, 1, 3, norm='none', act='tanh', pad_type=pad, use_sn=use_sn))
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class ContentEncoder(nn.Module):
+    def __init__(self, nf_cnt, n_downs, n_res, norm, act, pad, use_sn=False):
+        super(ContentEncoder, self).__init__()
+        print("Init ContentEncoder")
+
+        nf = nf_cnt
+
+        self.model = nn.ModuleList()
+        self.model.append(Conv2dBlock(3, nf, 7, 1, 3, norm=norm, act=act, pad_type=pad, use_sn=use_sn))
+
+        for _ in range(n_downs):
+            self.model.append(Conv2dBlock(nf, 2 * nf, 4, 2, 1, norm=norm, act=act, pad_type=pad, use_sn=use_sn))
+            nf *= 2
+        
+        self.model.append(ResBlocks(n_res, nf, norm=norm, act=act, pad_type=pad, use_sn=use_sn))
+
+        self.model = nn.Sequential(*self.model)
+        self.out_dim = nf
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class MLP(nn.Module):
+    def __init__(self, nf_in, nf_out, nf_mlp, num_blocks, norm, act, use_sn=False):
+        super(MLP, self).__init__()
+        self.model = nn.ModuleList()
+        nf = nf_mlp
+        self.model.append(LinearBlock(nf_in, nf, norm=norm, act=act, use_sn=use_sn))
+        for _ in range(num_blocks - 2):
+            self.model.append(LinearBlock(nf, nf, norm=norm, act=act, use_sn=use_sn))
+        self.model.append(LinearBlock(nf, nf_out, norm='none', act='none', use_sn=use_sn))
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x.view(x.size(0), -1))
+
+
+def weights_init(init_type='gaussian'):
+    def init_fun(m):
+        classname = m.__class__.__name__
+        if (classname.find('Conv') == 0 or classname.find(
+                'Linear') == 0) and hasattr(m, 'weight'):
+            if init_type == 'gaussian':
+                init.normal_(m.weight.data, 0.0, 0.02)
+            elif init_type == 'xavier':
+                init.xavier_normal_(m.weight.data, gain=math.sqrt(2))
+            elif init_type == 'kaiming':
+                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                init.orthogonal_(m.weight.data, gain=math.sqrt(2))
+            elif init_type == 'default':
+                pass
+            else:
+                assert 0, "Unsupported initialization: {}".format(init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant_(m.bias.data, 0.0)
+    return init_fun
+
+
+def assign_adain_params(adain_params, model):
+    # assign the adain_params to the AdaIN layers in model
+    for m in model.modules():
+        if m.__class__.__name__ == "AdaIN2d":
+            mean = adain_params[:, :m.num_features]
+            std = adain_params[:, m.num_features:2*m.num_features]
+            m.bias = mean.contiguous().view(-1)
+            m.weight = std.contiguous().view(-1)
+            if adain_params.size(1) > 2*m.num_features:
+                adain_params = adain_params[:, 2*m.num_features:]
+
+
+def get_num_adain_params(model):
+    # return the number of AdaIN parameters needed by the model
+    num_adain_params = 0
+    for m in model.modules():
+        if m.__class__.__name__ == "AdaIN2d":
+            num_adain_params += 2*m.num_features
+    return num_adain_params
+
+###################################################################################
+### GUIDING NET
+
+
+
+cfg = {
+    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+    'vgg19cut': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'N'],
+}
+
+
+class GuidingNet(nn.Module):
+    def __init__(self, img_size=64, output_k={'cont': 128, 'disc': 10}):
+        super(GuidingNet, self).__init__()
+        # network layers setting
+        self.features = make_layers(cfg['vgg11'], True)
+
+        self.disc = nn.Linear(512, output_k['disc'])
+        self.cont = nn.Linear(512, output_k['cont'])
+
+        self._initialize_weights()
+
+    def forward(self, x, sty=False):
+        x = self.features(x)
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        flat = x.view(x.size(0), -1)
+        cont = self.cont(flat)
+        if sty:
+            return cont
+        disc = self.disc(flat)
+        return {'cont': cont, 'disc': disc}
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+    def moco(self, x):
+        x = self.features(x)
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        flat = x.view(x.size(0), -1)
+        cont = self.cont(flat)
+        return cont
+
+    def iic(self, x):
+        x = self.features(x)
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        flat = x.view(x.size(0), -1)
+        disc = self.disc(flat)
+        return disc
+
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=False)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=False)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
