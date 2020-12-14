62a3f0ec0e5bffe37d20f7ee0c5d6458eac61044
Miles Gray <mgray16@gmail.com>
2020-12-14 14:31:26 -0600

    Fix datasets import

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,6 @@
-from .fractal import Fractal, FractalLabel, FractalLabelSR, \
-                     FractalTUNITContrastive
-from .fractal import make_fractal_alae_dataloader, \
-                     make_fractal_clr_dataloader, \
-                     make_fractal_clr_sr_dataloader, \
-                     make_fractal_TUNIT_dataloader
+from .fractal import Fractal, FractalLabel, FractalLabelSR
+from .fractal import FractalTUNITContrastive
+from .fractal import make_fractal_alae_dataloader
+from .fractal import make_fractal_clr_dataloader
+from .fractal import make_fractal_clr_sr_dataloader
+from .fractal import make_fractal_TUNIT_dataloader
