2254ddb5552542d5528ca29ef750d0b6627fc0c7
Miles Gray <mgray16@gmail.com>
2020-12-14 14:29:33 -0600

    Add TUNIT dataset stuff to datasets module

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,1 +1,6 @@
-from .fractal import Fractal, FractalLabel, FractalLabelSR, make_fractal_alae_dataloader, make_fractal_clr_dataloader, make_fractal_clr_sr_dataloader
+from .fractal import Fractal, FractalLabel, FractalLabelSR, \
+                     FractalTUNITContrastive
+from .fractal import make_fractal_alae_dataloader, \
+                     make_fractal_clr_dataloader, \
+                     make_fractal_clr_sr_dataloader, \
+                     make_fractal_TUNIT_dataloader
