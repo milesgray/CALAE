dea04d92c5cdbdaeaa0e6848aba919bde292e922
Miles Gray <miles@resatiate.com>
2022-08-14 19:24:58 -0500

    Fix imports

--- models/alae.py
+++ models/alae.py
@@ -14,9 +14,13 @@ from math import log2, ceil
 from torch.autograd import Function
 from torch.nn import functional as F
 
-from CALAE.layers import ToRGB, FromRGB, AdaIN
-from CALAE.layers import IntermediateNoise, Blur, AffineTransform
-from CALAE.layers.scaled import set_scale
+from CALAE.layers import Factory
+from CALAE.layers import ToRGB, FromRGB, AdaIN, AdaPN
+from CALAE.layers import IntermediateNoise, Blur
+from CALAE.layers import LearnableGaussianTransform2d, LearnableAffineTransform2d
+from CALAE.layers import ExplicitCoordConv
+from CALAE.layers import TripletAttention
+from CALAE.layers.scaled import ScaledConv2d, ScaledLinear, set_scale
 import CALAE.layers.lreq as lreq
 
 ####################################################################################################################
@@ -27,40 +31,41 @@ import CALAE.layers.lreq as lreq
 ############## E N C O D E R #############--------------------------------------------------------------------------
 
 class EncoderBlock(nn.Module):
-    def __init__(self, inp_c, oup_c, code, final=False, blur_downsample=False, fused_scale=True, learn_blur=False):
+    def __init__(self, inp_c, oup_c, code, scale, act='leaky', final=False, blur_downsample=False, fused_scale=True, learn_blur=False):
         super().__init__()
         
         self.final = final
         self.blur_downsample = blur_downsample
         self.learn_blur = learn_blur
 
-        self.learned_affine = AffineTransform(learnable=True, scale=oup_c if final else inp_c)
+        if self.learn_blur:
+            self.learned_affine = set_scale(LearnableAffineTransform2d(scale=(oup_c if final else inp_c, scale)))
         
         self.in1 = nn.InstanceNorm2d(inp_c, affine=False)
         self.in2 = nn.InstanceNorm2d(oup_c, affine=False)
         
-        self.conv1 = lreq.Conv2d(inp_c, inp_c, kernel_size=3, stride=1, padding=1)
-        self.style_mapping1 = lreq.Linear(2 * inp_c, code)
+        self.conv1 = ScaledConv2d(inp_c, inp_c, kernel_size=3, stride=1, padding=1)
+        self.style_mapping1 = ScaledLinear(2 * inp_c, code)
         
         if final:
-            self.fc = lreq.Linear(inp_c * 4 * 4, oup_c)
-            self.style_mapping2 = lreq.Linear(oup_c, code)
+            self.fc = ScaledLinear(inp_c * 4 * 4, oup_c)
+            self.style_mapping2 = ScaledLinear(code, oup_c)
         else:
-            self.conv2 = lreq.Conv2d(inp_c, oup_c, kernel_size=3, stride=1, padding=1)    
-            self.style_mapping2 = lreq.Linear(2 * oup_c, code)
+            self.conv2 = ScaledConv2d(inp_c, oup_c, kernel_size=3, stride=1, padding=1)    
+            self.style_mapping2 = ScaledLinear(2 * oup_c, code)
             
-        self.act = nn.LeakyReLU(0.2)
+        if act:
+            self.act = Factory.get_activation(act)
         self.downsample = nn.AvgPool2d(2, 2)
         
         self.blur = Blur(inp_c)
         
-    def forward(self, x, return_norm=False):
-        
+    def forward(self, x):
         x = self.act(self.conv1(x))
         statistics1 = torch.cat([x.mean(dim=[2,3]), x.std(dim=[2,3])], dim=1)
         style1 = self.style_mapping1(statistics1)
         x = self.in1(x)
-        if return_norm: norm = x
+        norm = x
         if self.final:
             x = x.view(x.shape[0], -1)
             statistics2 = self.act(self.fc(x))
@@ -77,72 +82,206 @@ class EncoderBlock(nn.Module):
         if not self.final:
             x = self.in2(x)
         
-        if return_norm: return x, style1, style2, norm
-        else: return x, style1, style2, None
+        return x, style1, style2, norm
 
 ####################################################################################################################
 ############ G E N E R A T O R ###########--------------------------------------------------------------------------
-    
 class GeneratorBlock(nn.Module):
-    def __init__(self, inp_c, oup_c, code, initial=False, blur_upsample=False, fused_scale=True, learn_blur=False, scale=4):
+    def __init__(self, inp_c, oup_c, code, initial=False, blur_upsample=True, 
+                 fused_scale=True, learn_blur=True, learn_residual=False, 
+                 learn_style=False, learn_noise=False, use_attn=True, use_coord=True,
+                 scale=4, act="leaky", norm="pixel"):
         super().__init__()
                 
         self.initial = initial
         self.blur_upsample = blur_upsample
         self.learn_blur = learn_blur
+        self.learn_residual = learn_residual
+        self.learn_style = learn_style
+        self.learn_noise = learn_noise
+        self.use_coord = use_coord
 
         # learnable affine transform to correct blur
         if self.learn_blur:
-            self.learned_affine = AffineTransform(learnable=True, scale=inp_c)
+            if self.initial:
+                self.blur_affine = set_scale(LearnableAffineTransform2d(scale=inp_c))
+            else:
+                self.blur_affine = set_scale(LearnableAffineTransform2d(scale=(inp_c, scale)))
+        if self.learn_residual:
+            self.residual_gain = nn.Parameter(torch.from_numpy(np.array([1, -1], dtype=np.float32)))
+            self.res_conv = lreq.Conv2d(inp_c, oup_c, kernel_size=3, stride=1, padding=1) # 
+            self.res_upsample = nn.UpsamplingBilinear2d(scale_factor=2)
+            self.res_ups_affine = LearnableGaussianTransform2d(scale=(oup_c, scale))
 
         # Learnable noise coefficients
         self.B1 = set_scale(IntermediateNoise(inp_c))
         self.B2 = set_scale(IntermediateNoise(oup_c))
+        # Learnable affine transform
+        if self.learn_noise:
+            self.B1_affine = LearnableGaussianTransform2d(scale=(inp_c, scale))
+            self.B2_affine = LearnableGaussianTransform2d(scale=(oup_c, scale))
+        if self.learn_style:
+            self.w1_affine = LearnableGaussianTransform0d(scale=code)
+            self.w2_affine = LearnableGaussianTransform0d(scale=code)
         
-        # Each Ada IN contains learnable parameters A
-        self.ada_in1 = AdaIN(inp_c, code)
-        self.ada_in2 = AdaIN(oup_c, code)
+        
+        # Each Ada PN contains learnable parameters A
+        if norm == "pixel":
+            self.ada_norm1 = AdaPN(inp_c, code)
+            self.ada_norm2 = AdaPN(oup_c, code)
+        elif norm == "instance":
+            self.ada_norm1 = AdaIN(inp_c, code)
+            self.ada_norm2 = AdaIN(oup_c, code)
+        
+        # In case if it is the initial block, learnable constant is created
+        if self.initial:            
+            self.constant = nn.Parameter(torch.randn(1, inp_c, 4, 4), requires_grad=True)
+            if self.use_coord:                
+                self.conv1 = ExplicitCoordConv(inp_c, inp_c, kernel_size=3, padding=1)
+        else:
+            if self.use_coord:                
+                self.conv1 = ExplicitCoordConv(inp_c, inp_c, kernel_size=3, padding=1)
+            else:
+                self.conv1 = lreq.Conv2d(inp_c, inp_c, kernel_size=3, padding=1)
+            
+        self.conv2 = lreq.Conv2d(inp_c, oup_c, kernel_size=3, padding=1)
+        
+        self.upsample = Upsample(inp_c) # nn.UpsamplingBilinear2d(scale_factor=2)
+        self.blur = Blur(inp_c)
+
+        if use_attn:
+            self.attn = TripletAttention()
+        
+        if act:
+            self.activation = Factory.get_activation(act)        
+        
+    def forward(self, x, w, bbox=None, e=None):
+        """
+        x - (N x C x H x W) -> N x C x H*2 x W*2
+        w - (N x C), where A: (N x C) -> (N x (C x 2))
+        e - (N x C x H x W)
+        """
+        i = x
+        if self.initial:
+            x = x.repeat(w.shape[0], 1, 1, 1)
+            if self.use_coord and bbox is not None:
+                x = self.conv1(x, bbox)
+        else:
+            x = self.upsample(x)            
+            
+            if self.blur_upsample:
+                x = self.blur(x)
+            if self.learn_blur:
+                x = self.blur_affine(x)
+
+            
+            if self.use_coord:
+                if bbox is not None:
+                    x = self.conv1(x, bbox)
+                else:
+                    raise ValueError("Must pass 'bbox' when using coord conv ('use_coord' = True)")
+            else:
+                x = self.conv1(x)
+            x = self.attn(x)
+            x = self.activation(x)    
+            
+        x = self.B1(x)
+        if self.learn_noise:
+            x = self.B1_affine(x)   
+        if self.learn_style:
+            w = self.w1_affine(w)
+        x = self.ada_norm1(x, w)        
+        x = self.activation(self.conv2(x))
+        
+        x = self.B2(x)
+        if self.learn_noise:
+            x = self.B2_affine(x)
+        if self.learn_style:
+            w = self.w2_affine(w)
+        x = self.ada_norm2(x, w)
+
+        if self.learn_residual and not self.initial:
+            i = self.res_conv(i)
+            i = self.res_upsample(i)
+            i = self.res_ups_affine(i)
+            ratio = F.softmax(self.residual_gain, dim=0)
+            x = x * ratio[0] + i * ratio[1]
+
+        return x
+
+   
+class GeneratorSkipBlock(nn.Module):
+    def __init__(self, inp_c, oup_c, code, blur_upsample=True, 
+                 fused_scale=True, learn_blur=True, learn_residual=False, 
+                 learn_style=False, learn_noise=False,
+                 scale=4, act="leaky", norm="pixel"):
+        super().__init__()
+
+        print(f"[GeneratorBlock]\t Input Channel: {inp_c}, Output Channel: {oup_c}, Code Size: {code}, Scale: {scale}")
+                
+        self.initial = initial
+        self.blur_upsample = blur_upsample
+        self.learn_blur = learn_blur
+        self.learn_residual = learn_residual
+        self.learn_style = learn_style
+        self.learn_noise = learn_noise
+
+        # learnable affine transform to correct blur
+        if self.learn_blur:
+            self.blur_affine = set_scale(LearnableAffineTransform2d(scale=(inp_c, scale)))
+        
+        self.residual_gain = nn.Parameter(torch.from_numpy(np.array([1, -1], dtype=np.float32)))
+        self.res_upsample = Upsample(inp_c) # nn.UpsamplingBilinear2d(scale_factor=2)
+        self.res_blur = Blur(inp_c)
+        if self.learn_blur:
+            self.res_blur_affine = set_scale(LearnableAffineTransform2d(scale=inp_c))    
+
+        # Learnable noise coefficients
+        self.B = set_scale(IntermediateNoise(inp_c))
+        # Learnable affine transform
+        if self.learn_noise:
+            self.B_affine = set_scale(LearnableAffineTransform2d(scale=(inp_c, scale)))
+        
+        # Each Ada PN contains learnable parameters A
+        if norm == "pixel":
+            self.norm = PixelNorm()
+        elif norm == "instance":
+            self.norm = nn.InstanceNorm2d()
         
         # In case if it is the initial block, learnable constant is created
         if self.initial:
             self.constant = nn.Parameter(torch.randn(1, inp_c, 4, 4), requires_grad=True)
         else:
-            self.conv1 = lreq.Conv2d(inp_c, inp_c, kernel_size=3, padding=1)
+            self.conv1 = ScaledConv2d(inp_c, inp_c, kernel_size=3, padding=1)
             
-        self.conv2 = lreq.Conv2d(inp_c, oup_c, kernel_size=3, padding=1)
+        self.conv2 = ScaledConv2d(inp_c, oup_c, kernel_size=3, padding=1)
         
-        self.upsample = nn.UpsamplingBilinear2d(scale_factor=2)
+        self.upsample = Upsample(inp_c) # nn.UpsamplingBilinear2d(scale_factor=2)
         self.blur = Blur(inp_c)
-        self.activation = nn.LeakyReLU(0.2)
+        if act:
+            self.activation = Factory.get_activation(act) 
         
-    def forward(self, x, w):
+    def forward(self, x):
         """
         x - (N x C x H x W)
         w - (N x C), where A: (N x C) -> (N x (C x 2))
         """
-        if self.initial:
-            x = x.repeat(w.shape[0], 1, 1, 1)            
-        else:
-            x = self.upsample(x)
-            x = self.conv1(x)
+        x = self.upsample(x)            
             
-            if self.blur_upsample:
-                x = self.blur(x)
-            if self.learn_blur:
-                x = self.learned_affine(x)
-            
-            x = self.activation(x)    
-            
-        x = self.B1(x)
-        if self.initial and self.learn_blur:
-            x = self.learned_affine(x)
+        if self.blur_upsample:
+            x = self.blur(x)
+        if self.learn_blur:
+            x = self.blur_affine(x)
+
+        x = self.conv1(x)
+        x = self.activation(x)    
         
-        x = self.ada_in1(x, w)        
-        x = self.activation(self.conv2(x))
-        
-        x = self.B2(x)
-            
-        return self.ada_in2(x, w)
+        x = self.B(x)
+        if self.learn_noise:
+            x = self.B_affine(x)   
+        x = self.norm(x)      
+
+        return x
     
 ####################################################################################################################
 ################################################## Level 3 blocks ##################################################
